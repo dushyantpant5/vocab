0cc4ccbd2eef0fc0e41cbbf2483e4f2269b9bd6c
Hridayansh Sah <87271258+Hirdiyansh@users.noreply.github.com>
2025-05-25 13:15:27 +0530

    added roue in dashboard words

--- src/app/api/words/checkLearnedTrue/[wordId]/route.ts
+++ src/app/api/words/checkLearnedTrue/[wordId]/route.ts
@@ -45,12 +45,13 @@ export async function PATCH(
 
     // After updating, invalidate the caches
     const cacheKeyAll = `learnedWords:${user.id}:all`;
+    const dashboardCacheKey = `dashboardWords:${user.id}:all`;
     const cacheKeyDate = `learned:${user.id}:${
       new Date().toISOString().split("T")[0]
     }`; // Using current date for cache invalidation
     await redis.del(cacheKeyAll); // Delete the cache for all learned words
     await redis.del(cacheKeyDate); // Delete the cache for date-specific learned words
-
+    await redis.del(dashboardCacheKey); //Delete dashboard cache if user click on mark as done from dashboard
     return NextResponse.json(updatedWordProgress);
   } catch {
     return NextResponse.json(
